8edcae46cf77ea48c0fb3893c8c62aee3ffea043
Andreas Jensen <andreas-mac@andreass-mbp.home>
2021-12-11 22:23:28 +0100

    solve day 2 part 2

--- day2.R
+++ day2.R
@@ -8,11 +8,19 @@ library(readr)
 library(tibble)
 
 ## PART 1 ##
-plan <- read_table("input_day_2.txt", col_names = c("direction", "magnitude"))
+plan <- read_table("input_day_2.txt", col_names = c("direction", "magnitude")) #Read input
 
-sums <- summarize(group_by(plan,direction),sum(magnitude))
-sums <- column_to_rownames(sums, var = "direction")
-(sums["down",]-sums["up",])*sums["forward",]
+sums <- summarize(group_by(plan, direction), sum(magnitude)) #sum magnitudes of moves for each direction
+sums <- column_to_rownames(sums, var = "direction") #rownames enable dictionary-like lookups
+(sums["down",] - sums["up",]) * sums["forward",] #calculate product of final depth and horizontal position
 
-
-
+## PART 2 ##
+plan <- mutate(plan,
+               aim_change = case_when(direction == "up" ~ -magnitude,
+                                      direction == "down" ~ magnitude,
+                                      T ~ 0), 
+               cum_aim = cumsum(aim_change),
+               depth_change = if_else(direction == "forward", magnitude*cum_aim, 0)
+)
+head(plan)
+summarize(plan, sum(depth_change)) * sums["forward",]
